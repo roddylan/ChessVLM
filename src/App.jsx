2b35e6e2944babcf08fd7820885adc520220310b
Roderick Lan <rodericklan03@gmail.com>
2025-03-16 01:49:07 -0600

    base

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Navbar from './components/Navbar.jsx'
 import CanvasComponent from './components/CanvasComponent.jsx'
 import GamePage from './components/GamePage.jsx'
-import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
