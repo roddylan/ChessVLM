e62f5d57a8d5446fa2fa290d57b76f2de04ffc3b
Roderick Lan <rodericklan03@gmail.com>
2025-03-16 15:19:35 -0600

    chessboard styling

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,11 +1,18 @@
 import './GamePage.css'
+import { Chessboard } from 'react-chessboard'
+
 
 export default function GamePage() {
     return (
         <div className="page">
-            <content id="main-content">
-                <h1>TEST</h1>
-            </content>
+            <main id="gamepage">
+                <div id="main-content">
+                    <h1 className='main-content-title'>TEST</h1>
+                </div>
+                <div id="cvl-board-div" className='inactive'>
+                    <Chessboard id="cvl-board" />
+                </div>
+            </main>
         </div>
     )
 }
