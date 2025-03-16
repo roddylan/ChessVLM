f0523dadaf9d9706c4907327f99abcf02190c4ba
Roderick Lan <rodericklan03@gmail.com>
2025-03-15 20:01:28 -0600

    css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav id="main-nav">
+            <h3 id="nav-title">CHESSvLM</h3>
+            <ul>
+                <li>Source</li>
+                <li>Guide</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
