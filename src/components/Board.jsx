ac62056dbfdcf17d5dda63767fa421440543ecac
Roderick Lan <rodericklan03@gmail.com>
2025-03-16 16:22:46 -0600

    started board setup

---
+++ src/components/Board.jsx
@@ -0,0 +1,151 @@
+export function Board() {
+    const [game, setGame] = useState(new Chess());
+    const [moveFrom, setMoveFrom] = useState("");
+    
+    const [moveTo, setMoveTo] = useState<Square | null>(null);
+
+    const [showPromotionDialog, setShowPromotionDialog] = useState(false);
+    const [rightClickedSquares, setRightClickedSquares] = useState({});
+    const [moveSquares, setMoveSquares] = useState({});
+    const [optionSquares, setOptionSquares] = useState({});
+
+    function safeGameMutate(modify) {
+        setGame(g => {
+            const update = {
+                ...g
+            };
+            modify(update);
+            return update;
+        });
+    }
+
+    function getMoveOptions(square) {
+        const moves = game.moves({
+            square, 
+            verbose: true
+        });
+        
+        if (moves.length === 0) {
+            setOptionSquares({});
+            return false;
+        }
+
+        const newSquares = {};
+        moves.map(move => {
+            newSquares[move.to] = {
+                background: game.get(move.to) && game.get(move.to).color !== game.get(square).color ? "radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)" : "radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)",
+                borderRadius: "50%"
+            };
+            return move;
+        });
+
+        newSquares[square] = {
+            background: "rgba(255, 255, 0, 0.4)"
+        };
+        setOptionSquares(newSquares);
+        return true;
+    }
+
+    function makeRandomMove() {
+        // TODO: change to get move from llm
+
+        const possibleMoves = game.moves();
+
+        // end if over
+        if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return;
+
+        const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+
+        safeGameMutate(game => {
+            game.move(possibleMoves[randomIndex]);
+        });
+    }
+
+    function onSquareClick(square) {
+        setRightClickedSquares({});
+
+        // from square
+        if (!moveFrom) {
+            const hasMoveOptions = getMoveOptions(square);
+            if (hasMoveOptions) {
+                setMoveFrom(square);
+            }
+            return;
+        }
+
+        // to square
+        if (!moveTo) {
+            // check if valid before showind dialog
+            const moves = game.moves({
+                moveFrom,
+                verbose: true
+            });
+            const foundMove = moves.find(m => m.from === moveFrom && m.to === square);
+
+            // invalid
+            if (!foundMove) {
+                // check if clicked on new piece
+                const hasMoveOptions = getMoveOptions(square);
+                // if new piece, setmovefrom, ow clear
+                setMoveFrom(hasMoveOptions ? square : "");
+                return;
+            }
+
+            // valid
+            setMoveTo(square);
+
+            if (foundMove.color === "w" && foundMove.piece === "p" && square[1] === "8" || 
+                foundMove.color === "b" && foundMove.piece === "p" && square[1] === "1") {
+                setShowPromotionDialog(true);
+                return;
+            }
+
+            // normal move
+            const gameCopy = {
+                ...game
+            };
+
+            const move = gameCopy.move({
+                from: moveFrom,
+                to: square,
+                promotion: "q"
+            });
+
+            // invalid -> setMoveFrom and getMoveOptions
+            if (move === null) {
+                const hasMoveOptions = getMoveOptions(square);
+                if (hasMoveOptions) setMoveFrom(square);
+                return;
+            }
+
+            setGame(gameCopy);
+            setTimeout(() => {
+                makeRandomMove 
+                // TODO: replace with llm, probably separate this function
+            }, 300);
+            setMoveTo(null)
+            setOptionSquares({});
+            return;
+        }
+    }
+    
+    function onPromotionPieceSelect(piece) {
+        // no piece passed -> cancel dialog; reset
+        if (piece) {
+            const gameCopy = {
+                ...game
+            };
+            gameCopy.move({
+                from: moveFrom,
+                to: moveTo,
+                promotion: piece[1].toLowerCase() ?? "q"
+            });
+            setGame(gameCopy);
+            setTimeout(makeRandomMove, 300);
+        }
+        setMoveFrom("");
+        setMoveTo(null);
+        
+    }
+}
+
--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react'
 import './GamePage.css'
 import { Chessboard } from 'react-chessboard'
-
+import { Chess } from 'chess.js'
 
 export default function GamePage() {
     return (
         <div className="page">
             <main id="gamepage">
                 <div id="main-content">
-                    <h1 className='main-content-title'>TEST</h1>
+                    <h1 className='main-content-title'>Chess Game</h1>
                 </div>
                 <div id="cvl-board-div" className='inactive'>
+                    {/* TODO: gray out when inactive */}
                     <Chessboard id="cvl-board" />
+                    {/* <div className='cvl-board-overlay inactive'></div> */}
                 </div>
             </main>
         </div>
