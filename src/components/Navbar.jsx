5a529ad6f59c4bf66be34d87983929c3a04c4db4
Roderick Lan <rodericklan03@gmail.com>
2025-03-16 13:37:12 -0600

    logo on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import './Navbar.css'
+import logoWhiteTransparent from '/ChessVLM/logo-white-transparent.svg'
 
 function Navbar() {
     return (
         <nav id="main-nav">
-            <h3 id="nav-title">CHESSvLM</h3>
+            <span id="nav-title">
+                <img src={ logoWhiteTransparent } alt="logo" id='logo' />
+                <h3>CHESSvLM</h3>
+            </span>
             <ul>
                 <li><a href="#" className='nav-btn'>Source</a></li>
                 <li><a href="#" className='nav-btn'>Guide</a></li>
