42433851aebdb90ce958a10ba0f085dc3ef505cf
Roderick Lan <rodericklan03@gmail.com>
2025-03-17 11:02:33 -0600

    character update

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -36,8 +36,8 @@ export default function CanvasComponent() {
                 fgColor="white"
                 // bgColor="transparent"
                 bgColor="#101010"
-                // characters="  .:-+*=%@#"
-                characters=" .:-+*=%@#"
+                characters="  .:-+*=%@#"
+                // characters=" .:-+*=%@#"
                 invert
                 renderIndex={1}
             />
